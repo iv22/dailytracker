809fbd03ba0172e3dc0f45bd12c60852f3f2b1bb
step <ask.ivankov@gmail.com>
2021-02-09 13:37:34 +0300

    Change User model and add seeds for Admin user role

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -10,15 +10,19 @@
 #  confirmed_at           :datetime
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
+#  first_name             :string           default(""), not null
+#  last_name              :string           default(""), not null
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
 #  role                   :integer          not null
+#  unconfirmed_email      :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
 # Indexes
 #
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #  index_users_on_role                  (role)
