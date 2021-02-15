ecd1fd9e5e0484ed760306045015db422abd0049
step <ask.ivankov@gmail.com>
2021-02-15 10:54:39 +0300

    Set up features for self registered Managers

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,4 +40,8 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+
+  def has_company?
+    !employee.nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   authenticated :user do
     root 'home#index', as: :authenticated_root
