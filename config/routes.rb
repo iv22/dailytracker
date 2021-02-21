fbe160434edd29a1ba272329d61f31424d25df18
Nataliya <an.chishevich@yandex.ru>
2021-02-21 16:52:53 +0300

    Fix logic in application_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  ActiveAdmin.routes(self)
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     confirmations: 'users/confirmations',
   }
+
   authenticated :user do
     root 'home#index', as: :authenticated_root
   end
+
   root 'home#index'  
+
+  ActiveAdmin.routes(self)
 end
