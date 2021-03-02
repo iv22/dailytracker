086090a8938d7c818c9a423313093aff9fae3ab9
step <ask.ivankov@gmail.com>
2021-03-02 17:02:45 +0300

    Move controllers_helper.rb to support folder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,7 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'controllers_helper'
+require 'support/controllers_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
