94b83aee509c7d25321c75dcac453421a0be0ec5
step <ask.ivankov@gmail.com>
2021-03-05 12:44:04 +0300

    Remove member rollback test

--- spec/services/employees/member_creator_spec.rb
+++ spec/services/employees/member_creator_spec.rb
@@ -19,32 +19,4 @@ RSpec.describe Employees::MemberCreator, type: :controller do
       expect(@member.company_user.company).to eq(company)
     end
   end
-
-  context 'negative scenario' do
-    def execute_ddl(ddl)
-      ActiveRecord::Base.connection.execute(ddl)
-    end
-
-    def simulate_db_trouble
-      execute_ddl 'ALTER TABLE company_users ADD COLUMN breaker integer NOT NULL;'
-    end
-
-    def undo_db_trouble
-      execute_ddl 'ALTER TABLE company_users DROP COLUMN IF EXISTS breaker;'
-    end
-
-    before(:all) do
-      undo_db_trouble
-      simulate_db_trouble
-    end
-
-    after(:all) do
-      undo_db_trouble
-    end
-
-    it 'should raise error and should not add an Employee' do
-      expect { Employees::MemberCreator.call(params, company) }.to raise_error(Exception)
-      expect(User.find_by(email: params[:email])).to be_nil
-    end
-  end
 end
