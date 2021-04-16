ffdef15d3b116e7941fc40296e33c328ec806387
step <ask.ivankov@gmail.com>
2021-04-16 12:12:34 +0300

    Change Employees list view for scrollable

--- frontend/components/Pages/Employee/List/index.jsx
+++ frontend/components/Pages/Employee/List/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import EmployeesListActions from './Action';
 import EmployeeAddEdit from '../AddEdit';
 import axios from 'init/api';
+import "./style";
 import { PersonIcon, DimPlusIcon, ActiveIcon, InvitedIcon, LockedIcon } from "components/General/Icons";
 
 const EmployeesList = () => {
@@ -45,46 +46,46 @@ const EmployeesList = () => {
       {isEmployeeOpen && <EmployeeAddEdit id={id} handleModalClose={() => setIsEmployeeOpen(false)} updatedAt={setUpdatedAt} />}
 
       {error ? <div className="alert alert-danger" role="alert">{error}</div> :
-        <table className="e-index-table">
-          <thead className="e-index-head">
-            <tr>
-              <th className="e-icon-head"></th>
-              <th className="e-name-head">Name</th>
-              <th className="e-email-head">Email</th>
-              <th className="e-phone-head">Phone</th>
-              <th className="e-role-head">Role</th>
-              <th className="e-status-head">Status</th>
-              <th className="e-action-head">Action</th>
-            </tr>
-          </thead>
-          <tbody className="e-index-body">
-            <tr className="e-action-row">
-              <td colSpan="3" className="e-icon">
-                <div className="e-circle e-add-person-circle e-clickable"
-                  onClick={() => handleAddEdit()}>
-                  <DimPlusIcon className="e-circle e-add-person-icon" />
-                  <span className="e-add-person-text">Add&nbsp;employee</span>
-                </div>
-              </td>
-            </tr>
-            {employees && employees.map((emp) => (
-              <tr className="e-index-row" key={emp.id}>
-                <td className="e-icon">
-                  <div className="e-circle e-person-circle"></div>
-                  <PersonIcon className="e-person-icon" />
-                </td>
-                <td>{emp.first_name + ' ' + emp.last_name}</td>
-                <td>{emp.email}</td>
-                <td>{emp.phone}</td>
-                <td>{emp.role}</td>
-                <td className="text-center">{getStatusIcon(emp.status)}</td>
-                <td className="e-data-cell">
-                  <EmployeesListActions handleEdit={() => handleAddEdit(emp.id)} />
-                </td>
+        <React.Fragment>
+          <table className="e-index-table">
+            <thead className="e-index-head">
+              <tr>
+                <th className="e-icon-col"></th>
+                <th className="e-name-col">Name</th>
+                <th className="e-email-col">Email</th>
+                <th className="e-phone-col">Phone</th>
+                <th className="e-role-col">Role</th>
+                <th className="e-status-col">Status</th>
+                <th className="e-action-col">Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="e-index-body">
+              {employees && employees.map((emp) => (
+                <tr className="e-index-row" key={emp.id}>
+                  <td className="e-icon e-icon-col">
+                    <div className="e-circle e-person-circle"></div>
+                    <PersonIcon className="e-person-icon" />
+                  </td>
+                  <td className="e-name-col">{emp.first_name + ' ' + emp.last_name}</td>
+                  <td className="e-email-col">{emp.email}</td>
+                  <td className="e-phone-col">{emp.phone}</td>
+                  <td className="e-role-col">{emp.role}</td>
+                  <td className="text-center e-status-col">{getStatusIcon(emp.status)}</td>
+                  <td className="e-data-cell e-action-col">
+                    <EmployeesListActions handleEdit={() => handleAddEdit(emp.id)} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="e-action-row">
+            <div className="e-circle e-add-person-circle e-clickable"
+              onClick={() => handleAddEdit()}>
+              <DimPlusIcon className="e-circle e-add-person-icon" />
+              <span className="e-add-person-text">Add&nbsp;employee</span>
+            </div>
+          </div>
+        </React.Fragment>
       }
     </React.Fragment>
   )
